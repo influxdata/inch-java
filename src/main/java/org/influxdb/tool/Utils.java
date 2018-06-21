5d11065542c56b12aa23753acf9bd05fce188193
Hoan Xuan Le <hoan.le@bonitoo.io>
2018-06-21 12:31:13 +0700

    QueryComparator to compare the query time between different InfluxDBImpl configs

---
+++ src/main/java/org/influxdb/tool/Utils.java
@@ -0,0 +1,106 @@
+package org.influxdb.tool;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.TestUtils;
+import org.influxdb.InfluxDB.ResponseFormat;
+import org.influxdb.dto.Pong;
+
+import okhttp3.OkHttpClient;
+
+public class Utils {
+  private static String getEnv(String name, String defaultValue) {
+    Map<String, String> getenv = System.getenv();
+
+    if (getenv.containsKey(name)) {
+      return getenv.get(name);
+    } else {
+      return defaultValue;
+    }
+  }
+  
+  public static String getInfluxIP() {
+    return getEnv("INFLUXDB_IP", "127.0.0.1");
+  }
+  
+  public static String getRandomMeasurement() {
+    return "measurement_" + System.nanoTime();
+  }
+  
+  public static String getInfluxPORT(boolean apiPort) {
+    if(apiPort) {    
+      return getEnv("INFLUXDB_PORT_API", "8086");
+    }
+    else {
+      return getEnv("INFLUXDB_PORT_COLLECTD", "8096");
+    }      
+  }
+
+  public static String getProxyApiUrl() {
+    return getEnv("PROXY_API_URL", "http://127.0.0.1:8086/");
+  }
+
+  public static String getProxyUdpPort() {
+    return getEnv("PROXY_UDP_PORT", "8089");
+  }
+
+  public static String defaultRetentionPolicy(String version) {
+    if (version.startsWith("0.") ) {
+      return "default";
+    } else {
+      return "autogen";
+    }
+  }
+
+  public static InfluxDB connectToInfluxDB() throws InterruptedException, IOException {
+    return connectToInfluxDB(null, null, ResponseFormat.JSON);
+  }
+
+  public static InfluxDB connectToInfluxDB(ResponseFormat responseFormat) throws InterruptedException, IOException {
+    return connectToInfluxDB(null, null, responseFormat);
+  }
+  public static InfluxDB connectToInfluxDB(String apiUrl) throws InterruptedException, IOException {
+    return connectToInfluxDB(new OkHttpClient.Builder(), apiUrl, ResponseFormat.JSON);
+  }
+  
+  public static InfluxDB connectToInfluxDB(final OkHttpClient.Builder client, String apiUrl,
+      ResponseFormat responseFormat) throws InterruptedException, IOException {
+    OkHttpClient.Builder clientToUse;
+    if (client == null) {
+      clientToUse = new OkHttpClient.Builder();
+    } else {
+      clientToUse = client;
+    }
+    clientToUse.readTimeout(0, TimeUnit.MILLISECONDS);
+    String apiUrlToUse; 
+    if (apiUrl == null) {
+      apiUrlToUse = "http://" + getInfluxIP() + ":" + getInfluxPORT(true);
+    } else {
+      apiUrlToUse = apiUrl;
+    }
+    InfluxDB influxDB = InfluxDBFactory.connect(apiUrlToUse, "admin", "admin", clientToUse, responseFormat);
+    boolean influxDBstarted = false;
+    do {
+      Pong response;
+      try {
+        response = influxDB.ping();
+        if (response.isGood()) {
+          influxDBstarted = true;
+        }
+      } catch (Exception e) {
+        // NOOP intentional
+        e.printStackTrace();
+      }
+      Thread.sleep(100L);
+    } while (!influxDBstarted);
+    influxDB.setLogLevel(InfluxDB.LogLevel.NONE);
+    System.out.println("##################################################################################");
+    System.out.println("#  Connected to InfluxDB Version: " + influxDB.version() + " #");
+    System.out.println("##################################################################################");
+    return influxDB;
+  }
+}
---
+++ src/main/java/org/influxdb/tool/benchmark/QueryComparator.java
@@ -0,0 +1,31 @@
+package org.influxdb.tool.benchmark;
+
+import java.io.IOException;
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDB.ResponseFormat;
+import org.influxdb.dto.Query;
+import org.influxdb.tool.Utils;
+
+public class QueryComparator {
+
+  public static void main(String[] args) throws InterruptedException, IOException {
+    System.out.println("response format = " + args[0].toUpperCase());
+    InfluxDB influxDB = Utils.connectToInfluxDB(ResponseFormat.valueOf(args[0].toUpperCase()));
+    
+    long now = System.currentTimeMillis();
+    String statement = "select * from m0 where tag0='value0'";
+    influxDB.query(new Query(statement, "j_stress"));
+    long elapsed = System.currentTimeMillis() - now;
+    System.out.println("statement = " + statement + ", elapsed = " + elapsed);
+
+    now = System.currentTimeMillis();
+    statement = "select * from m0";
+    influxDB.query(new Query(statement, "j_stress"));
+    elapsed = System.currentTimeMillis() - now;
+
+    System.out.println("statement = " + statement + ", elapsed = " + elapsed);
+  }
+  
+  
+}
